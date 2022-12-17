1d4cf84f1b85dfafc056b7c1319808798a9271f9
appert <scoreur@gmail.com>
2022-12-17 11:39:33 +0100

    mise en place de la logique

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="links">
+        <RouterLink to="/" active-class="active">Home</RouterLink>
+        <RouterLink to="/about" active-class="active">About</RouterLink>
+    </div>
+  <RouterView />
+</template>
+
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+
+<style scoped>
+.active {
+  color: green
+}
+</style>
